ed085dac29785d6f4032164db318accfa5e8b3e1
Wayne Gong <nju_gw@msn.com>
2018-02-10 13:47:01 +0800

    add first implementation

--- LogDemo/LogManager/LogManager.cs
+++ LogDemo/LogManager/LogManager.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Serilog;
+using Serilog.Core;
 
 namespace LogManager
 {
     public class LogManager : ILogManager
     {
+        private readonly Logger _logger;
+
+        protected static Logger DefaultLogger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        internal LogManager(Logger logger)
+        {
+            _logger = logger ?? DefaultLogger;
+        }
+
+        public LogManager()
+            : this(null)
+        { }
+
         public void LogInfo(string message)
         {
-            throw new NotImplementedException();
+            _logger.Information(message);
         }
     }
 }
